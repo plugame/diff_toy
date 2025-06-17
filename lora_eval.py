45651b3a85bed592448e0d45b9ae9637dceb6824
plugame <plugame.jp@gmail.com>
2025-06-17 16:23:40 +0900

    fix

--- lora_eval.py
+++ lora_eval.py
@@ -1,79 +1,84 @@
 import os
 import torch
-from diffusers import UNet2DConditionModel, DDPMScheduler, AutoencoderKL, DDIMScheduler,EulerAncestralDiscreteScheduler
+from diffusers import UNet2DConditionModel, DDPMScheduler, AutoencoderKL, DDIMScheduler, EulerAncestralDiscreteScheduler, DPMSolverMultistepScheduler
 from transformers import CLIPTokenizer, CLIPTextModel
 from tqdm import tqdm
-from safetensors.torch import load_file
 #original
-from utils.utils import encode_prompt, prepare_empty_latent, decode_latents,inject_lora_from_pretrained, convert_injectable_dict_from_weight
+from safetensors.torch import load_file
+from utils.utils import encode_prompt, prepare_empty_latent,decode_latents,convert_injectable_dict_from_weight,inject_lora_from_pretrained
 from utils.diffusers_utils import convert_injectable_dict_from_khoya_weight
-
-
-device = torch.device("cuda:0")
+device = torch.device("cuda")
 dtype = torch.float16
 
 # base_model
-model_path = "E:\\lab\\program\\train_controlnet\\diffusers_model\\1450_v9"
+model_path = "/nfs2/sandbox/mori/train_controlnet/diffusers_model/v1-5-pruned-emaonly"
+
+#lora_path = "/nfs2/sandbox/mori/diff_toy/lora_output/test_lora.safetensors"
+lora_path = "/nfs2/sandbox/mori/ComfyUI/models/loras/only_color_v1.safetensors"
+lora_scale = 0.8
+original_lora = False
+
 
 # output
 output_dir = "generate"
 os.makedirs(output_dir,exist_ok=True)
 
-# lora
-lora_path = r"E:\sd\Lora\bocchi_style_offset.safetensors"
-lora_scale = 0.8
-original_lora = False
-
 # parameter
+sampling_steps=20 
 guidance_scale = 8
-sampling_steps=20
-width,height = 512, 768
+width,height= 512,512
 
 
-prompts =  " bocchi style, gotou hitori, 1girl, blue eyes, cube hair ornament, hair between eyes, hair ornament, hair over eyes, jacket, long hair, one side up, open mouth, pink hair, pink jacket, solo, track jacket, zipper, indoors, masterpiece"
-negative_prompt = "low quality, worst quality, blurry, extra limbs"
 
-"""
 prompts = "base_color,only_color,anime,little_witch"
 negative_prompt = None
-"""
 
-# load models
+
+#----------------------------------------
+
 tokenizer = CLIPTokenizer.from_pretrained(f"{model_path}/tokenizer",local_files_only=True)
 text_encoder = CLIPTextModel.from_pretrained(f"{model_path}/text_encoder",torch_dtype=dtype).to(device)
 unet = UNet2DConditionModel.from_pretrained(model_path, subfolder="unet", torch_dtype=dtype).to(device)
 vae = AutoencoderKL.from_pretrained(model_path, subfolder="vae", torch_dtype=dtype).to(device)
-scheduler = DDPMScheduler.from_pretrained(model_path, subfolder="scheduler")
-#scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_path, subfolder="scheduler")
-scheduler.set_timesteps(sampling_steps) 
-
+#scheduler = DDIMScheduler.from_pretrained(model_path, subfolder="scheduler", beta_schedule='scaled_linear',beta_start=0.00085,beta_end=0.0120,eta=0.0, torch_dtype=dtype)
+#scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_path,subfolder="scheduler")
+#scheduler = DPMSolverMultistepScheduler.from_pretrained(model_path,subfolder="scheduler",use_karras_sigmas=True)
+scheduler = DPMSolverMultistepScheduler.from_pretrained(
+    model_path,
+    subfolder="scheduler",
+    algorithm_type="sde-dpmsolver++",
+    use_karras_sigmas = True
+)
+scheduler.set_timesteps(sampling_steps)  
 
 # load lora
 weights = load_file(lora_path)
 
+# kohya_ss系から
 unet_lora_dict, te_lora_dict, network_alphas = convert_injectable_dict_from_weight(weights) if original_lora else convert_injectable_dict_from_khoya_weight(weights)
+
 
 # weight scale lora 
 for key, weight in network_alphas.items():
     network_alphas[key] = weight * lora_scale
 
-# inject lora
+
 inject_lora_from_pretrained(unet,unet_lora_dict,network_alphas)
 inject_lora_from_pretrained(text_encoder,te_lora_dict,network_alphas)
 
+#------------------------------------------
 
-# generate
 positive_embeds, negative_embeds = encode_prompt(prompts, tokenizer, text_encoder,negative_prompt=negative_prompt)
 prompt_embeds = torch.cat([negative_embeds, positive_embeds])
+
 latents = prepare_empty_latent(width,height,scheduler,device,dtype)
 
 for i, t in enumerate(tqdm(scheduler.timesteps.to(device))):
     with torch.no_grad():
-        #latent_input = latents
-
         latent_input = torch.cat([latents]*2)
         latent_input = scheduler.scale_model_input(latent_input, t)
 
+        # 2回UNetに通す
         noise_pred = unet(
             latent_input,
             t, 
@@ -86,5 +91,5 @@ for i, t in enumerate(tqdm(scheduler.timesteps.to(device))):
 
         latents = scheduler.step(noise_pred, t, latents).prev_sample
 
-    image = decode_latents(latents,vae)
-    image.save(f"{output_dir}/gen_{i}.png")
+    latent = decode_latents(latents,vae)
+    latent.save(f"{output_dir}/gen_{i}.png")
